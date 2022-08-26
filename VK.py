95f1ad8d47af7af9defef6659d14f55cba73ce23
Loreanna <s.s.khovanskaya@gmail.com>
2022-08-26 12:11:06 +0300

    Second version

--- VK.py
+++ VK.py
@@ -9,12 +9,13 @@ class VKUser:
             'owner_id': owner_id  
         }
 
-    def get_photos_info(self, album_id):
+    def get_photos_info(self, album_id, count):
         URL = "https://api.vk.com/method/photos.get"
         get_photos_params = {
             'album_id': album_id,
             'extended': 1,
-            'photo_sizes': 1
+            'photo_sizes': 1,
+            'count': count
         }
         res = requests.get(URL, params={**self.params, **get_photos_params}).json()
         return res
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import configparser
 import json
 
-import pyprind
+from tqdm import tqdm
 
 from VK import VKUser
 from YaDisk import YandexDisk
 
 
-def get_dict_of_links_and_names(all_pictures_info_json):
+def get_dict_of_links_and_names(pictures_info_json):
     types_dict = {"s": 1, "m": 2, "x": 3, "o": 4, "p": 5, "q": 6, "r": 7, "y": 8, "z": 9, "w": 10}  
     dict_of_links_and_names = {}
-    for photo in all_pictures_info_json["response"]["items"]:
+    for photo in pictures_info_json["response"]["items"]:
         grade = 0
         type = ""
         url = ""
@@ -33,7 +33,7 @@ def get_dict_of_links_and_names(all_pictures_info_json):
         dict_of_links_and_names[name]["type"] = type
     return dict_of_links_and_names
 
-def upload_photos_from_VK_to_YaDisk(VKUser_ID, path_to_folder, album_id, n=5):
+def upload_photos_from_VK_to_YaDisk(VKUser_ID, path_to_folder, album_id, count=5):
 
     config = configparser.ConfigParser()
     config.read("tokens.ini")
@@ -45,30 +45,19 @@ def upload_photos_from_VK_to_YaDisk(VKUser_ID, path_to_folder, album_id, n=5):
     YaDiskUser = YandexDisk(TOKEN_YADISK)
 
     YaDiskUser.create_folder(path_to_folder)
-    json_info = UserVK.get_photos_info(album_id)
+    json_info = UserVK.get_photos_info(album_id, count)
     json_short_dict = get_dict_of_links_and_names(json_info)
     
-    if n > len(json_short_dict):
-        i = len(json_short_dict)
-    else:
-        i = n
-    bar = pyprind.ProgBar(i, width=200)
-
-    list_of_files = []
-    counter = 0
-    for filename, info in json_short_dict.items():      
+    list_of_files = []  
+    for filename, info in tqdm(json_short_dict.items()):      
         url = info["url"]
         path_on_yadisk = f'{path_to_folder}/{filename}'
         YaDiskUser.upload_file_by_link(url, path_on_yadisk)
         list_of_files.append({"filename": filename, "size": info["type"]})
-        counter += 1
-        bar.update()    
-        if counter == n:
-            break
-        
+            
     with open("logs.json", "w") as f:
         json.dump(list_of_files, f, indent=4)
     return
 
 if __name__ == '__main__':
-    upload_photos_from_VK_to_YaDisk("1", "testfolder", "profile", n=10)
+    upload_photos_from_VK_to_YaDisk("728964058", "testfolder", "profile", count=4)
